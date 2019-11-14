c398c778f2c2f2e667cf936e34ae938e673e3c28
fumiaki.sato <fumis@mac.com>
2019-11-14 13:07:56 +0000

    kaizou

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,58 +1,49 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
-  def index
-    @tasks = Task.all
-  end
-
-  def show
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def create
-    @task = Task.new(task_params)
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:update, :destroy]
     
+  def create
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task　が正常に登録されました'
-      redirect_to @task
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task　が登録されませんでした'
-      render :new
+      @tasks = current_user.feed_tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render 'toppages/index'
     end
   end
 
-  def edit
-  end
-
   def update
-    if @task.update(task_params)
-      flash[:success] = 'Task　は正常に更新されました'
-      redirect_to @task
+    @task = current_user.tasks update(id: :desc).page(params[:page])
+    if @task.save
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to root_url
     else
-      flash[:success] = 'Task　は更新されませんでした'
-      render :edit
+      @task = current_user.tasks update(id: :desc).page(params[:page])
+      flash[:danger] = 'タスクは更新されませんでした'
+      render 'toppages/index'
     end
   end
 
   def destroy
     @task.destroy
-    
-    flash[:success] = 'Task　は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_112033) do
+ActiveRecord::Schema.define(version: 2019_11_06_092335) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
